df1eec3f543c94435b3720436c2c9d3a0b19f580
Brshin <bradyshinners@gmail.com>
2018-07-09 13:17:00 -0500

    portfolio button functional

--- global_assets/javascript/home.js
+++ global_assets/javascript/home.js
@@ -1,10 +1,42 @@
 $(document).ready(function () {
     var about = false, portfolio = false, contact = false;
+    var portfolioOpts = [];
+
+    function buildPortfolio() {
+        function Card(filepath, imagepath, altName, titleName) {
+            this.page = filepath;
+            this.image = imagepath;
+            this.alt = altName;
+            this.title = titleName;
+        };
+
+        var rpg = new Card("pages/rpg/rpg-game.html", "pages/rpg/assets/images/portrait/roy.jpg", "Fire Emblem RPG", "RPG Game");
+        var psychic = new Card("pages/psychic/psychic.html", "pages/psychic/assets/images/ball.jpeg", "Pyschic Ball", "Psychic Game");
+        var trivia = new Card("pages/trivia/trivia.html", "pages/trivia/assets/images/dragontrivia.jpg", "Dragon", "Myth Trivia");
+        var bearCrystal = new Card("pages/crystal_math/crystalgame.html", "pages/crystal_math/assets/images/bear.jpg", "Bear", "Honey Bear");
+        var gifResponse = new Card("pages/gifer/gif-response.html", "pages/gifer/assets/images/hobbiton.jpg", "hobbiton", "Gifs of the Rings");
+        var trainTime = new Card("pages/traintimer/traintime.html", "pages/traintimer/assets/images/traintrax.jpg", "train tracks", "Train Station");
+        var crawlspace = new Card("pages/crawlspace/crawlspace.html", "pages/crawlspace/assets/images/background.jpg", "binary", "Crawlspace");
+
+        portfolioOpts.push(rpg, psychic, trivia, bearCrystal, gifResponse, trainTime, crawlspace);
+    };
+
+    function clickCarrot(div) {
+        $(div).prepend("< ");
+        $(div).append(" >");
+        $(div).css("color", "gold");
+    };
+
+    function cleanCarrot(div, cleanText) {
+        $(div).text(cleanText)
+        $(div).removeAttr("style")
+    }
 
     function aboutFiller(section) {
         var newDiv;
         if (!about) {
             about = true;
+            clickCarrot("#aboutClick");
 
             newDiv = $("<div>");
             newDiv.addClass("abouttext")
@@ -20,36 +52,63 @@ $(document).ready(function () {
         } else {
             about = false;
             $(section).empty();
+            cleanCarrot("#aboutClick", "About")
         };
     };
 
     function portFiller(section) {
-        var newDiv = $("<div>");
         if (!portfolio) {
             portfolio = true;
+            clickCarrot("#portClick");
+
+            //var columns = 0;
+
+            var rowDiv = $("<div>");
+            rowDiv.addClass("row");
 
             for (var i = 0; i < portfolioOpts.length; i++) {
+                var colDiv = $("<div>");
+                colDiv.addClass("col-lg-4 col-sm-6 col-6 centerSmall");
 
-            }
+                var portImage = $("<img>");
+                portImage.addClass("portfolioimages");
+                portImage.attr("src", portfolioOpts[i].image);
+                portImage.attr("alt", portfolioOpts[i].alt);
+
+                var portText = $("<p>");
+                portText.addClass("portfoliotext");
+                portText.html(portfolioOpts[i].title);
+
+                var aLink = $("<a>");
+                aLink.attr("href", portfolioOpts[i].page);
+
+                aLink.append(portImage,portText)
+                colDiv.append(aLink);
+                rowDiv.append(colDiv);
+                $(section).html(rowDiv);
+            };
+        } else {
+            portfolio = false;
+            $(section).empty();
+            cleanCarrot("#portClick", "Portfolio");
         }
-    }
-
-    function fillSection(section) {
-
-        switch (section) {
-            case "#aboutFill":
-                aboutFiller(section);
-                break;
-
-            case "portFill":
-                break;
-
-            case "conFill":
-                break;
-        };
     };
 
+    function conFiller(section) {
+
+    }
+
     $("#aboutClick").on("click", function () {
-        fillSection("#aboutFill");
+        aboutFiller("#aboutFill");
     });
+
+    $("#portClick").on("click", function () {
+        portFiller("#portFill");
+    });
+
+    $("#conClick").on("click", function () {
+        conFiller("#contactFill");
+    });
+
+    buildPortfolio();
 });
