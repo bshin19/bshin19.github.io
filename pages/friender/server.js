3da993dc5c1340cc45b387e40c52bb2f24c6f292
Brady Shinners <brady.shinners@gmail.com>
2018-08-05 22:24:45 -0500

    survey page done. Match time

--- pages/friender/server.js
+++ pages/friender/server.js
@@ -1,5 +1,10 @@
 /// GLOBAL VARIABLES ///
+
 var express = require('express');
+var exphbs = require('express-handlebars')
+
+// Create an instance of the express app.
+var app = express();
+
 var bodyParser = require('body-parser');
 var path = require('path');
-
